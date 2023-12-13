0582e6de4aaecd1ac0a50174b1d24e5cbebed7a0
Marian Beuther <mb@peakconcepts.de>
2023-12-13 17:48:43 +0100

    get part2 running

--- src/2023/13.test.ts
+++ src/2023/13.test.ts
@@ -1,8 +1,12 @@
+import { readFileSync } from 'fs'
 import {
-  getHorizontalReflectionLine,
   getSetsFromInput,
-  getSumOfInputReflections,
+  getHorizontalReflectionLine,
   getVerticalReflectionLine,
+  getSumOfInputReflections,
+  getHorizontalReflectionLineWithSmudge,
+  getVerticalReflectionLineWithSmudge,
+  getSumOfInputReflectionsWithSmudges,
 } from './13'
 
 const testInput = `#.##..##.
@@ -21,8 +25,11 @@ const testInput = `#.##..##.
 ..##..###
 #....#..#`
 
-describe('day13Part01', () => {
-  it('should return the correct sets from input', () => {
+const testSets = getSetsFromInput(
+  readFileSync('src/2023/inputs/13.txt', 'utf8'),
+)
+describe('getSetsFromInput', () => {
+  it('should return an array of sets from input', () => {
     const expectedSets = [
       `#.##..##.
 ..#.##.#.
@@ -41,28 +48,90 @@ describe('day13Part01', () => {
     ]
     expect(getSetsFromInput(testInput)).toEqual(expectedSets)
   })
+})
 
+describe('getHorizontalReflectionLine', () => {
   it('should return the correct horizontal reflection line', () => {
-    expect(getHorizontalReflectionLine(getSetsFromInput(testInput)[1])).toEqual(
-      400,
-    )
     expect(getHorizontalReflectionLine(getSetsFromInput(testInput)[0])).toEqual(
       0,
     )
+    expect(getHorizontalReflectionLine(getSetsFromInput(testInput)[1])).toEqual(
+      400,
+    )
   })
+})
 
+describe('getVerticalReflectionLine', () => {
   it('should return the correct vertical reflection line', () => {
     expect(getVerticalReflectionLine(getSetsFromInput(testInput)[0])).toEqual(5)
     expect(getVerticalReflectionLine(getSetsFromInput(testInput)[1])).toEqual(0)
   })
+})
 
-  it('should return the correct result for our testinput', () => {
+describe('getSumOfInputReflections', () => {
+  it('should return the correct sum of input reflections', () => {
     expect(getSumOfInputReflections(testInput)).toEqual(405)
   })
 })
 
-describe('day13Part02', () => {
-  it('should return the correct result', () => {
-    // Add your test case here
+describe('getHorizontalReflectionLineWithSmudge', () => {
+  it('should return the correct horizontal reflection line with smudge', () => {
+    expect(
+      getHorizontalReflectionLineWithSmudge(getSetsFromInput(testInput)[0]),
+    ).toEqual(300)
+    expect(
+      getHorizontalReflectionLineWithSmudge(getSetsFromInput(testInput)[1]),
+    ).toEqual(100)
+    expect(getHorizontalReflectionLineWithSmudge(testSets[0])).toEqual(0)
+    expect(getHorizontalReflectionLineWithSmudge(testSets[1])).toEqual(300)
+    expect(getHorizontalReflectionLineWithSmudge(testSets[2])).toEqual(100)
+    expect(getHorizontalReflectionLineWithSmudge(testSets[3])).toEqual(0)
+    expect(getHorizontalReflectionLineWithSmudge(testSets[4])).toEqual(200)
+    expect(getHorizontalReflectionLineWithSmudge(testSets[5])).toEqual(0)
+    expect(getHorizontalReflectionLineWithSmudge(testSets[6])).toEqual(0)
+    expect(getHorizontalReflectionLineWithSmudge(testSets[7])).toEqual(0)
+    expect(getHorizontalReflectionLineWithSmudge(testSets[8])).toEqual(1000)
+    expect(getHorizontalReflectionLineWithSmudge(testSets[9])).toEqual(700)
+    expect(getHorizontalReflectionLineWithSmudge(testSets[10])).toEqual(200)
+    expect(getHorizontalReflectionLineWithSmudge(testSets[11])).toEqual(0)
+    expect(getHorizontalReflectionLineWithSmudge(testSets[12])).toEqual(0)
+    expect(getHorizontalReflectionLineWithSmudge(testSets[13])).toEqual(0)
+    expect(getHorizontalReflectionLineWithSmudge(testSets[65])).toEqual(700)
+    expect(getHorizontalReflectionLineWithSmudge(testSets[82])).toEqual(300)
+    expect(getHorizontalReflectionLineWithSmudge(testSets[86])).toEqual(0)
+  })
+})
+
+describe('getVerticalReflectionLineWithSmudge', () => {
+  it('should return the correct vertical reflection line with smudge', () => {
+    expect(
+      getVerticalReflectionLineWithSmudge(getSetsFromInput(testInput)[0]),
+    ).toEqual(0)
+    expect(
+      getVerticalReflectionLineWithSmudge(getSetsFromInput(testInput)[1]),
+    ).toEqual(0)
+    expect(getVerticalReflectionLineWithSmudge(testSets[0])).toEqual(8)
+    expect(getVerticalReflectionLineWithSmudge(testSets[1])).toEqual(0)
+    expect(getVerticalReflectionLineWithSmudge(testSets[2])).toEqual(0)
+    expect(getVerticalReflectionLineWithSmudge(testSets[3])).toEqual(5)
+    expect(getVerticalReflectionLineWithSmudge(testSets[4])).toEqual(0)
+    expect(getVerticalReflectionLineWithSmudge(testSets[5])).toEqual(1)
+    expect(getVerticalReflectionLineWithSmudge(testSets[6])).toEqual(12)
+    expect(getVerticalReflectionLineWithSmudge(testSets[7])).toEqual(8)
+    expect(getVerticalReflectionLineWithSmudge(testSets[8])).toEqual(0)
+    expect(getVerticalReflectionLineWithSmudge(testSets[9])).toEqual(0)
+    expect(getVerticalReflectionLineWithSmudge(testSets[10])).toEqual(0)
+    expect(getVerticalReflectionLineWithSmudge(testSets[11])).toEqual(6)
+    expect(getVerticalReflectionLineWithSmudge(testSets[12])).toEqual(7)
+    expect(getVerticalReflectionLineWithSmudge(testSets[13])).toEqual(5)
+    expect(getVerticalReflectionLineWithSmudge(testSets[65])).toEqual(0)
+    expect(getVerticalReflectionLineWithSmudge(testSets[82])).toEqual(0)
+    expect(getVerticalReflectionLineWithSmudge(testSets[86])).toEqual(5)
+  })
+})
+
+describe('getSumOfInputReflectionsWithSmudges', () => {
+  it('should return the correct sum of input reflections with smudges', () => {
+    expect(getSumOfInputReflectionsWithSmudges(testInput)).toEqual(400)
   })
 })
--- src/2023/13.ts
+++ src/2023/13.ts
@@ -1,4 +1,5 @@
 import { readFile } from 'fs/promises'
+import { get } from 'http'
 
 export const getSetsFromInput = (input: string): string[] => {
   return input.split('\n\n')
@@ -23,8 +24,57 @@ export const isMapReflection = (
   return isMapReflection(binaryMap, startIndex - 1, width + 2)
 }
 
+export const getDuplicateNeighbourIndexes = (binaryMap: number[]): number[] => {
+  return binaryMap
+    .reduce((acc, currentValue, index) => {
+      if (currentValue === binaryMap[index + 1]) {
+        acc.push(index)
+      }
+      return acc
+    }, [] as number[])
+    .filter((index) => {
+      if (index !== -1 && isMapReflection(binaryMap, index)) {
+        return true
+      }
+      return false
+    })
+}
+
+export const getSmudgeFixedBinaryMaps = (binaryMap: number[]): number[][] => {
+  return binaryMap.reduce((acc, currentValue, currentIndex) => {
+    const potentialMirrorIndexes = binaryMap
+      .map((_, i) => i)
+      .filter((mirrorIndex) => {
+        if (binaryMap[mirrorIndex] === currentValue) return false
+        // check if its off by 1 bit
+        return (
+          (binaryMap[mirrorIndex] ^ currentValue).toString(2).match(/1/g)
+            ?.length === 1
+        )
+      })
+    potentialMirrorIndexes.forEach((potentialMirrorIndex) => {
+      if (potentialMirrorIndex !== -1) {
+        const binaryMapWithSmudgeFix = [...binaryMap]
+        binaryMapWithSmudgeFix[currentIndex] = binaryMap[potentialMirrorIndex]
+        acc.push(binaryMapWithSmudgeFix)
+      }
+    })
+    return acc
+  }, [])
+}
+
+export const getReflectionLineSum = (
+  binaryMap: number[],
+  exclude: number[] = [],
+): number => {
+  const duplicateNeighbourIndexes = getDuplicateNeighbourIndexes(
+    binaryMap,
+  ).filter((d) => !exclude.includes(d))
+
+  return duplicateNeighbourIndexes.reduce((sum, index) => (sum += index + 1), 0)
+}
+
 export const getHorizontalReflectionLine = (input: string): number => {
-  let reflectionLineSum = 0
   const lines = input.split('\n')
   const binaryMapped = lines.map((line) =>
     line.split('').reduce((acc, char, index) => {
@@ -34,25 +84,10 @@ export const getHorizontalReflectionLine = (input: string): number => {
       return acc
     }, 0),
   )
-  const duplicateNeighbourIndexes = binaryMapped.reduce((acc, v, index) => {
-    if (v === binaryMapped[index + 1]) {
-      acc.push(index)
-    }
-    return acc
-  }, [] as number[])
-  duplicateNeighbourIndexes.forEach((duplicateNeighbourIndex) => {
-    if (
-      duplicateNeighbourIndex !== -1 &&
-      isMapReflection(binaryMapped, duplicateNeighbourIndex)
-    ) {
-      reflectionLineSum += 100 * (duplicateNeighbourIndex + 1)
-    }
-  })
-  return reflectionLineSum
+  return getReflectionLineSum(binaryMapped) * 100
 }
 
 export const getVerticalReflectionLine = (input: string): number => {
-  let reflectionLineSum = 0
   const lines = input.split('\n')
   const verticalLines = lines[0]
     .split('')
@@ -65,22 +100,7 @@ export const getVerticalReflectionLine = (input: string): number => {
       return acc
     }, 0),
   )
-  const duplicateNeighbourIndexes = binaryMapped.reduce((acc, v, index) => {
-    if (v === binaryMapped[index + 1]) {
-      acc.push(index)
-    }
-    return acc
-  }, [] as number[])
-
-  duplicateNeighbourIndexes.forEach((duplicateNeighbourIndex) => {
-    if (
-      duplicateNeighbourIndex !== -1 &&
-      isMapReflection(binaryMapped, duplicateNeighbourIndex)
-    ) {
-      reflectionLineSum += duplicateNeighbourIndex + 1
-    }
-  })
-  return reflectionLineSum
+  return getReflectionLineSum(binaryMapped)
 }
 export const getSumOfInputReflections = (input: string): number => {
   const sets = getSetsFromInput(input)
@@ -91,6 +111,68 @@ export const getSumOfInputReflections = (input: string): number => {
   }, 0)
 }
 
+export const getHorizontalReflectionLineWithSmudge = (
+  input: string,
+): number => {
+  const lines = input.split('\n')
+  const binaryMap = lines.map((line) =>
+    line.split('').reduce((acc, char, index) => {
+      if (char === '#') {
+        return acc + Math.pow(2, index)
+      }
+      return acc
+    }, 0),
+  )
+
+  const binaryMapsWithSmudgeFixes = getSmudgeFixedBinaryMaps(binaryMap)
+  const baseNeighbourIndexes = getDuplicateNeighbourIndexes(binaryMap)
+
+  return (
+    Math.max(
+      ...binaryMapsWithSmudgeFixes.map((m) =>
+        getReflectionLineSum(m, baseNeighbourIndexes),
+      ),
+      0,
+    ) * 100
+  )
+}
+
+export const getVerticalReflectionLineWithSmudge = (input: string): number => {
+  const lines = input.split('\n')
+  const verticalLines = lines[0]
+    .split('')
+    .map((_, colIndex) => lines.map((line) => line[colIndex]))
+  const binaryMap = verticalLines.map((line) =>
+    line.reduce((acc, char, index) => {
+      if (char === '#') {
+        return acc + Math.pow(2, index)
+      }
+      return acc
+    }, 0),
+  )
+  const binaryMapsWithSmudgeFixes = getSmudgeFixedBinaryMaps(binaryMap)
+  const baseNeighbourIndexes = getDuplicateNeighbourIndexes(binaryMap)
+
+  return Math.max(
+    ...binaryMapsWithSmudgeFixes.map((m) =>
+      getReflectionLineSum(m, baseNeighbourIndexes),
+    ),
+    0,
+  )
+}
+
+export const getSumOfInputReflectionsWithSmudges = (input: string): number => {
+  const sets = getSetsFromInput(input)
+  const log = []
+  const res = sets.reduce((sum, set) => {
+    const horizontalReflectionLine = getHorizontalReflectionLineWithSmudge(set)
+    const verticalReflectionLine = getVerticalReflectionLineWithSmudge(set)
+    log.push(horizontalReflectionLine + verticalReflectionLine)
+    return sum + horizontalReflectionLine + verticalReflectionLine
+  }, 0)
+  return res
+}
+
 export const partOne = async (): Promise<number> => {
   const input = await readFile('src/2023/inputs/13.txt', 'utf8')
   return getSumOfInputReflections(input)
@@ -98,5 +180,7 @@ export const partOne = async (): Promise<number> => {
 
 export const partTwo = async (): Promise<number> => {
   const input = await readFile('src/2023/inputs/13.txt', 'utf8')
-  return 0
+  const split = getSetsFromInput(input)
+
+  return getSumOfInputReflectionsWithSmudges(input)
 }
