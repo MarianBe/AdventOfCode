e77fe0d5f9ba0fe268369a8d2f14a950be2b69fc
Marian Beuther <mb@peakconcepts.de>
2023-12-21 14:10:10 +0100

    rename tests

--- src/2023/21.test.ts
+++ src/2023/21.test.ts
@@ -13,13 +13,13 @@ const testInput = `...........
 ...........`
 
 describe('day21Part01', () => {
-  it('should ', () => {
+  it('should find the availble nodes for our steps ', () => {
     expect(getReachableNodesAmount(testInput, 6)).toEqual(16)
   })
 })
 
 describe('day21Part02', () => {
-  it('should', () => {
+  it('should find the available steps with an infinte grid', () => {
     expect(getReachableNodesAmount(testInput, 50, true)).toEqual(1594)
     expect(getReachableNodesAmount(testInput, 100, true)).toEqual(6536)
   })
